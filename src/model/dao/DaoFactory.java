c38d34a23a6c65b208d93ab7fadec67bcfc4d714
Lucas Garcia <lukas.garcia52@gmail.com>
2020-02-16 13:24:02 -0300

    FindById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,11 @@
 package model.dao;
 
-public class DaoFactory {
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
 
+public class DaoFactory {
+	
+	public static SellerDao CreateSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
 }
